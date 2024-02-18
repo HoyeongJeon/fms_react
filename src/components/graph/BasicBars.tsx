1e8175103e37f1e1780976378e86dd5b895dddda
kim <iufan1526@gmail.com>
2024-02-18 19:26:29 +0900

    update : 화면 디자인 개선

--- src/components/graph/BasicBars.tsx
+++ src/components/graph/BasicBars.tsx
@@ -1,35 +1,35 @@
-import { BarChart } from "@mui/x-charts/BarChart";
-import { YellowAndRedCardsType } from "pages/Team";
+import { BarChart } from '@mui/x-charts/BarChart';
+import { YellowAndRedCardsType } from 'pages/Team';
 
 export default function BasicBars(props: YellowAndRedCardsType) {
-  const colors = ["#FFC107", "#FF0000"];
+    const colors = ['#FFC107', '#FF0000'];
 
-  const date = props.yellowAndRedCards.map((item) => {
-    return item.created;
-  });
+    const date = props.yellowAndRedCards.map((item) => {
+        return item.created;
+    });
 
-  const yellowCards = props.yellowAndRedCards.map((item) => {
-    return item.yellow;
-  });
+    const yellowCards = props.yellowAndRedCards.map((item) => {
+        return Math.floor(item.yellow);
+    });
 
-  const redCards = props.yellowAndRedCards.map((item) => {
-    return item.red;
-  });
+    const redCards = props.yellowAndRedCards.map((item) => {
+        return Math.floor(item.red);
+    });
 
-  return (
-    <div className="bar-main-container">
-      <BarChart
-        xAxis={[
-          {
-            scaleType: "band",
-            data: date,
-          },
-        ]}
-        series={[{ data: yellowCards }, { data: redCards }]}
-        width={500}
-        height={300}
-        colors={colors}
-      />
-    </div>
-  );
+    return (
+        <div className="bar-main-container">
+            <BarChart
+                xAxis={[
+                    {
+                        scaleType: 'band',
+                        data: date,
+                    },
+                ]}
+                series={[{ data: yellowCards }, { data: redCards }]}
+                width={500}
+                height={300}
+                colors={colors}
+            />
+        </div>
+    );
 }
